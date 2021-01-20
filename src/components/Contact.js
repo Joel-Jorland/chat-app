d6741b4fe7b61312a8b230e2f2640f862cec0109
Joël Jorland <joel.jorland@gmail.com>
2021-01-20 12:27:29 +0100

    Jojo's contact challenge

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './contact.css';
+
+
+function Contact() {
+    return (
+        <div className="Contact">
+            <img className="avatar" src="https://randomuser.me/api/portraits/men/88.jpg" />
+            <div className="status">
+                <div className="name">John Michael</div>
+                <div className="status-online"> </div>
+                <div className="status-text">Online</div>
+                
+            </div>
+        </div>
+    );
+  }
+
+export default Contact; 
