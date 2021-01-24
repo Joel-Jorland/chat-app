6d6ecd19d999aeeef50f1b410f84a34f4c2aa423
Joël Jorland <joel.jorland@gmail.com>
2021-01-24 21:39:37 +0100

    contact-list branch added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-     <Contact name="Joe Exotic" avatar="https://randomuser.me/api/portraits/men/36.jpg" online />
-     <Contact name="Ted Cruz" avatar="https://randomuser.me/api/portraits/men/73.jpg" offline />
-     <Contact name="Jean-Marie" avatar="https://randomuser.me/api/portraits/men/0.jpg" online />
+     <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import './contact.css';
-import PropTypes from 'prop-types';
+import React from 'react'
+import './contact.css'// Import du contact css
 
-
-function Contact(props) {
+function Contact(props){
     return (
         <div className="Contact">
-            <img className="avatar" src={props.avatar} />
-                <div>
-                    <p className="name">{props.name}</p>
-                        <div className="status">
-                        <div className={props.online ? "status-online" : "status-offline"}/>
-                    <p className="status-text">{props.online ? "online" : "offline"}</p>
-                </div>
-            </div>
+        <img className="avatar" src={props.avatar} alt={props.name} />
+        <div>
+          <p className="name">{props.name}</p>
+          <div className="status">
+            <div className={props.online ? "status-online" : "status-offline"} />
+            <p className="status-text">
+              {props.online ? "online" : "offline"}
+            </p>
+          </div>
         </div>
-    );
-  }
+      </div>
+    )
+}
 
-  Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired, 
-  };
-  
-  export default Contact; 
+export default Contact
