1535a6db5e0058e60ab385cb5d5bda71f95a353b
Joël Jorland <joel.jorland@gmail.com>
2021-01-26 18:32:23 +0100

    Chat-app is operational

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import './contact.css'// Import du contact css
 
-function Contact(props){
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      online: props.online,
+    }
+  }
+  render (){
     return (
         <div className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.name} />
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
         <div>
-          <p className="name">{props.name}</p>
-          <div className="status">
-            <div className={props.online ? "status-online" : "status-offline"} />
+          <p className="name">{this.props.name}</p>
+          <div className='status'
+            onClick={event => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+              }}>
+            <div className={this.state.online ? "status-online" : "status-offline"} />
             <p className="status-text">
-              {props.online ? "online" : "offline"}
+              {this.state.online ? "online" : "offline"}
             </p>
           </div>
         </div>
       </div>
     )
+  }
 }
 
 export default Contact
